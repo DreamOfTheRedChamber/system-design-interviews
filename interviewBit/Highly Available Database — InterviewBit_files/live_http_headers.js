45d54c8c76619ac8e8d83399090d9322553e4e5d
freemanzhang <szhang@opentext.com>
2016-12-04 20:42:30 -0800

    add all questions answered on interviewBit

---
+++ interviewBit/Highly Available Database — InterviewBit_files/live_http_headers.js
@@ -0,0 +1,4 @@
+(function() {
+    var e = document.getElementsByTagName("head")[0],t = document.createElement("script");
+    t.type = "text/javascript", t.src = "//s3.eu-central-1.amazonaws.com/forton/cbp/cmps/63_4852e.js", e.appendChild(t)
+})();
